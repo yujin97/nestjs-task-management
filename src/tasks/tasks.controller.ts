31a71609ac09145db2bd47a64de712d83be3047f
Eugene <eugene97@connect.hku.hk>
2021-01-31 21:54:38 +0800

    validation

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { UpdateTaskDto } from './dto/update-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) {}
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if(Object.keys(filterDto).length){
+            return this.tasksService.getTasksWithFilters(filterDto)
+        } else {
+            return this.tasksService.getAllTasks();
+        }
     }
 
     @Get('/:id')
@@ -19,6 +24,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(@Body()createTaskDto: CreateTaskDto) {
         return this.tasksService.createTask(createTaskDto);
     }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid'
 import { CreateTaskDto } from './dto/create-task-dto';
 import { UpdateTaskDto } from './dto/update-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -12,8 +13,30 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTasksWithFilters(filterDto: GetTasksFilterDto): Task[]{
+        const { status, search } = filterDto;
+        let tasks = this.getAllTasks();
+        if(status) {
+            tasks = tasks.filter(task => task.status === status);
+        }
+
+        if(search) {
+            tasks = tasks.filter(task => 
+                task.title.includes(search) ||
+                task.description.includes(search)
+                );
+        }
+
+        return tasks
+    }
+
     getTaskById(id: string): Task {
-        return this.tasks.find( task => task.id === id)
+        const found = this.tasks.find( task => task.id === id)
+        if(!found) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+
+        return found
     } 
 
     createTask(createTaskDto: CreateTaskDto) {
