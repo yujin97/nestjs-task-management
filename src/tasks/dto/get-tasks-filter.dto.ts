aa37577ea5f3a6c208aa8362bcebba277b1ebebc
Eugene <eugene97@connect.hku.hk>
2021-01-31 23:15:36 +0800

    param pipe

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsOptional, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+    @IsOptional()
+    @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     status: TaskStatus;
+
+    @IsOptional()
+    @IsNotEmpty()
     search: string;
 }
